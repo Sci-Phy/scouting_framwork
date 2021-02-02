30c875a8d04943f307fc3a9232ea90b0cc28e2c8
Sci-Phy <22405937+Sci-Phy@users.noreply.github.com>
2021-02-02 16:49:05 -0500

    Add Analysis page
    Working at this point in time

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Analysis from "../views/Analysis.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/analysis",
+    name: "Analysis",
+    component: Analysis
   },
   {
     path: "/about",
